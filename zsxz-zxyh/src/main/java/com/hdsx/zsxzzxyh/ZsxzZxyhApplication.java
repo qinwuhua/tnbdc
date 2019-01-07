6849559ec8f9767915b725963a5b873c04d3db0f
lirt <540491950@qq.com>
2019-01-07 10:20:39 +0800

    专项养护模块模板搭建

---
+++ zsxz-zxyh/src/main/java/com/hdsx/zsxzzxyh/ZsxzZxyhApplication.java
@@ -0,0 +1,14 @@
+package com.hdsx.zsxzzxyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZsxzZxyhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZsxzZxyhApplication.class, args);
+    }
+
+}
+
