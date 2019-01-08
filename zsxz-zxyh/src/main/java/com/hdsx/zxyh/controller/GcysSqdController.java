88cb7ef56d7ae3ef3ce1c5cc6dfb536590ee719e
qwh <1665942885@qq.com>
2019-01-08 14:09:00 +0800

    工程验收申请单

--- zsxz-zxyh/src/main/java/com/hdsx/zxyh/controller/GcysSqdController.java
+++ zsxz-zxyh/src/main/java/com/hdsx/zxyh/controller/GcysSqdController.java
@@ -2,10 +2,11 @@ package com.hdsx.zxyh.controller;
 
 
 import com.github.pagehelper.PageInfo;
+import com.hdsx.zxyh.entity.Gcyssqd;
 import com.hdsx.zxyh.entity.Gczfgcjld;
 import com.hdsx.zxyh.entity.Gczfzqcwzf;
 import com.hdsx.zxyh.entity.Msg;
-import com.hdsx.zxyh.service.GczfService;
+import com.hdsx.zxyh.service.GcysSqdService;
 import com.hdsx.zxyh.utils.ResultUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -22,21 +23,48 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("zxyh/gczf")
-@Api(value = "工程支付控制器",description = "工程支付控制器")
-public class GczfController {
+@Api(value = "工程验收申请单控制器",description = "工程验收申请单控制器")
+public class GcysSqdController {
 
     @Resource
-    private GczfService gczfService;
+    private GcysSqdService gcysSqdService;
 
 
-    @RequestMapping(value = "getHtXxInfoByBm", method = RequestMethod.GET, produces = "application/json")
-    @ApiOperation(value = "获取合同详细信息(工程计量单模块使用)")
-    @ApiImplicitParam(paramType="query", dataType = "String", name = "htbh", value = "合同编号", required = false)
-    public Msg getHtXxInfoByBm(@RequestParam(value = "htbh",required = false) String htbh){
+
+    @RequestMapping(value = "getGcyssqdList", method = RequestMethod.GET, produces = "application/json")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType="query", dataType = "String", name = "dwbh", value = "单位编号", required = false),
+            @ApiImplicitParam(paramType="query", dataType = "String", name = "htbh", value = "合同编号", required = false),
+            @ApiImplicitParam(paramType="query", dataType = "String", name = "ksrq", value = "申请开始日期(yyyy/mm/dd)", required = false),
+            @ApiImplicitParam(paramType="query", dataType = "String", name = "jsrq", value = "申请结束日期(yyyy/mm/dd)", required = false),
+            @ApiImplicitParam(paramType="query", dataType = "int", name = "pageNum", value = "页码", required = true),
+            @ApiImplicitParam(paramType="query", dataType = "int", name = "pageSize", value = "每页条数", required = true)
+    })
+    @ApiOperation(value = "查询工程验收申请单列表数据")
+    public Msg getGcyssqdList(@RequestParam(value = "dwbh",required = false) String dwbh,
+                            @RequestParam(value = "htbh",required = false) String htbh,
+                            @RequestParam(value = "ksrq",required = false) String ksrq,
+                            @RequestParam(value = "jsrq",required = false) String jsrq,
+                            @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                            @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+        Map<String, String> param = new HashMap<String, String>();
+        param.put("dwbh", dwbh);
+        param.put("htbh", htbh);
+        param.put("ksrq", ksrq);
+        param.put("jsrq", jsrq);
+        List<Gcyssqd> list = gcysSqdService.getGcyssqdList(param, pageNum, pageSize);
+        return ResultUtil.success(new PageInfo<Gcyssqd>(list));
+    }
+
+
+    @RequestMapping(value = "getGcyssqdInfoBySqbh", method = RequestMethod.GET, produces = "application/json")
+    @ApiOperation(value = "通过申请编号获取工程验收申请单详细信息")
+    @ApiImplicitParam(paramType="query", dataType = "String", name = "sqbh", value = "申请编号", required = true)
+    public Msg getGcyssqdInfoBySqbh(@RequestParam(value = "sqbh",required = true) String sqbh){
         try {
             Map<String, String> param = new HashMap<String, String>();
-            param.put("htbh", htbh);
-            List<Gczfgcjld> map=gczfService.getHtXxInfoByBm(param);
+            param.put("sqbh", sqbh);
+            Gcyssqd map=gcysSqdService.getGcyssqdInfoBySqbh(param);
             return ResultUtil.success(map);
         }catch (Exception e){
             e.printStackTrace();
@@ -44,7 +72,64 @@ public class GczfController {
         }
 
     }
-    @RequestMapping(value = "getGcjldList", method = RequestMethod.GET, produces = "application/json")
+
+    @RequestMapping(value = "addGcyssqdForGcys", method = RequestMethod.POST, produces = "application/json")
+    @ApiOperation(value = "添加工程验收申请单数据")
+    public Msg addGcyssqdForGcys(@RequestBody Gcyssqd gcyssqd){
+        try {
+            int flag=gcysSqdService.addGcyssqdForGcys(gcyssqd);
+            if(flag>0)
+                return ResultUtil.success("保存成功");
+            else
+                return ResultUtil.error("保存失败");
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResultUtil.error("保存失败，接口异常");
+        }
+
+    }
+
+
+    @RequestMapping(value = "editGcyssqdForGcys", method = RequestMethod.PUT, produces = "application/json")
+    @ApiOperation(value = "编辑工程验收申请单数据")
+    public Msg editGcjldForGczf(@RequestBody Gcyssqd gcyssqd){
+        try {
+            int flag=gcysSqdService.editGcyssqdForGcys(gcyssqd);
+            if(flag>0)
+                return ResultUtil.success("保存成功");
+            else
+                return ResultUtil.error("保存失败");
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResultUtil.error("保存失败，接口异常");
+        }
+
+    }
+
+
+    @RequestMapping(value = "delGcyssqdForGcys", method = RequestMethod.DELETE, produces = "application/json")
+    @ApiOperation(value = "删除工程验收申请单数据")
+    @ApiImplicitParam(paramType="query", dataType = "String", name = "sqbhs", value = "申请编号(以“,”隔开)", required = true)
+    public Msg delGcyssqdForGcys(@RequestParam(value = "sqbhs",required = true) String sqbhs){
+        try {
+            String[] djbh=sqbhs.split(",");
+            List<String> l = new ArrayList<String>();
+            for (int i=0;i<djbh.length;i++){
+                l.add(djbh[i]);
+            }
+            int flag=gcysSqdService.delGcyssqdForGcys(l);
+            if(flag>0)
+                return ResultUtil.success("删除成功");
+            else
+                return ResultUtil.error("删除失败");
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResultUtil.error("删除失败，接口异常");
+        }
+
+    }
+
+   /* @RequestMapping(value = "getGcjldList", method = RequestMethod.GET, produces = "application/json")
     @ApiImplicitParams({
             @ApiImplicitParam(paramType="query", dataType = "String", name = "dwbh", value = "单位编号", required = false),
             @ApiImplicitParam(paramType="query", dataType = "String", name = "gcjl_htbh", value = "合同编号", required = false),
@@ -65,18 +150,18 @@ public class GczfController {
         param.put("gcjl_htbh", gcjl_htbh);
         param.put("ksrq", ksrq);
         param.put("jsrq", jsrq);
-        List<Gczfgcjld> list = gczfService.getGcjldList(param, pageNum, pageSize);
+        List<Gczfgcjld> list = gcysSqdService.getGcjldList(param, pageNum, pageSize);
         return ResultUtil.success(new PageInfo<Gczfgcjld>(list));
     }
 
     @RequestMapping(value = "getGcjldInfoByDjbh", method = RequestMethod.GET, produces = "application/json")
     @ApiOperation(value = "通过单据编号获取工程计量单详细信息")
-    @ApiImplicitParam(paramType="query", dataType = "String", name = "djbh", value = "单据编号", required = true)
+    @ApiImplicitParam(paramType="query", dataType = "String", name = "djbh", value = "支付期号", required = true)
     public Msg getGcjldInfoByDjbh(@RequestParam(value = "djbh",required = true) String djbh){
         try {
             Map<String, String> param = new HashMap<String, String>();
             param.put("djbh", djbh);
-            Gczfgcjld map=gczfService.getGcjldInfoByDjbh(param);
+            Gczfgcjld map=gcysSqdService.getGcjldInfoByDjbh(param);
             return ResultUtil.success(map);
         }catch (Exception e){
             e.printStackTrace();
@@ -89,7 +174,7 @@ public class GczfController {
     @ApiOperation(value = "添加工程计量单数据")
     public Msg addGcjldForGczf(@RequestBody Gczfgcjld gczfgcjld){
         try {
-            int flag=gczfService.addGcjldForGczf(gczfgcjld);
+            int flag=gcysSqdService.addGcjldForGczf(gczfgcjld);
             if(flag>0)
                 return ResultUtil.success("保存成功");
             else
@@ -106,7 +191,7 @@ public class GczfController {
     @ApiOperation(value = "编辑工程计量单数据")
     public Msg editGcjldForGczf(@RequestBody Gczfgcjld gczfgcjld){
         try {
-            int flag=gczfService.editGcjldForGczf(gczfgcjld);
+            int flag=gcysSqdService.editGcjldForGczf(gczfgcjld);
             if(flag>0)
                 return ResultUtil.success("保存成功");
             else
@@ -129,7 +214,7 @@ public class GczfController {
             for (int i=0;i<djbh.length;i++){
                 l.add(djbh[i]);
             }
-            int flag=gczfService.delGcjldForGczf(l);
+            int flag=gcysSqdService.delGcjldForGczf(l);
             if(flag>0)
                 return ResultUtil.success("删除成功");
             else
@@ -154,7 +239,7 @@ public class GczfController {
             }
             Map<String,Object> param=new HashMap<String,Object>();
             param.put("spzt",spzt);param.put("djbhs",l);
-            int flag=gczfService.spGcjldForGczf(param);
+            int flag=gcysSqdService.spGcjldForGczf(param);
             if(flag>0)
                 return ResultUtil.success("审批成功");
             else
@@ -176,7 +261,7 @@ public class GczfController {
         try {
             Map<String, String> param = new HashMap<String, String>();
             param.put("htbh", htbh);
-            List<Gczfzqcwzf> map=gczfService.getHtCwInfoByBm(param);
+            List<Gczfzqcwzf> map=gcysSqdService.getHtCwInfoByBm(param);
             return ResultUtil.success(map);
         }catch (Exception e){
             e.printStackTrace();
@@ -208,7 +293,7 @@ public class GczfController {
         param.put("htbh", htbh);
         param.put("ksrq", ksrq);
         param.put("jsrq", jsrq);
-        List<Gczfzqcwzf> list = gczfService.getZqcwzfList(param, pageNum, pageSize);
+        List<Gczfzqcwzf> list = gcysSqdService.getZqcwzfList(param, pageNum, pageSize);
         return ResultUtil.success(new PageInfo<Gczfzqcwzf>(list));
     }
 
@@ -219,7 +304,7 @@ public class GczfController {
         try {
             Map<String, String> param = new HashMap<String, String>();
             param.put("zfqh", zfqh);
-            Gczfzqcwzf map=gczfService.getZqcwzfInfoByZfqh(param);
+            Gczfzqcwzf map=gcysSqdService.getZqcwzfInfoByZfqh(param);
             return ResultUtil.success(map);
         }catch (Exception e){
             e.printStackTrace();
@@ -233,7 +318,7 @@ public class GczfController {
     @ApiOperation(value = "添加中(终)期财务支付证书数据")
     public Msg addZqcwzfForGczf(@RequestBody Gczfzqcwzf gczfzqcwzf){
         try {
-            int flag=gczfService.addZqcwzfForGczf(gczfzqcwzf);
+            int flag=gcysSqdService.addZqcwzfForGczf(gczfzqcwzf);
             if(flag>0)
                 return ResultUtil.success("保存成功");
             else
@@ -250,7 +335,7 @@ public class GczfController {
     @ApiOperation(value = "编辑中(终)期财务支付证书数据")
     public Msg editZqcwzfForGczf(@RequestBody Gczfzqcwzf gczfzqcwzf){
         try {
-            int flag=gczfService.editZqcwzfForGczf(gczfzqcwzf);
+            int flag=gcysSqdService.editZqcwzfForGczf(gczfzqcwzf);
             if(flag>0)
                 return ResultUtil.success("保存成功");
             else
@@ -272,7 +357,7 @@ public class GczfController {
             for (int i=0;i<zfqh.length;i++){
                 l.add(zfqh[i]);
             }
-            int flag=gczfService.delZqcwzfForGczf(l);
+            int flag=gcysSqdService.delZqcwzfForGczf(l);
             if(flag>0)
                 return ResultUtil.success("删除成功");
             else
@@ -297,7 +382,7 @@ public class GczfController {
             }
             Map<String,Object> param=new HashMap<String,Object>();
             param.put("spzt",spzt);param.put("djbhs",l);
-            int flag=gczfService.spZqcwzfForGczf(param);
+            int flag=gcysSqdService.spZqcwzfForGczf(param);
             if(flag>0)
                 return ResultUtil.success("审批成功");
             else
@@ -309,6 +394,6 @@ public class GczfController {
 
     }
 
-
+*/
 
 }
---
+++ zsxz-zxyh/src/main/java/com/hdsx/zxyh/service/impl/GcysSqdServiceImpl.java
@@ -0,0 +1,50 @@
+package com.hdsx.zxyh.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.hdsx.zxyh.entity.Gcyssqd;
+import com.hdsx.zxyh.entity.Gczfgcjld;
+import com.hdsx.zxyh.entity.Gczfzqcwzf;
+import com.hdsx.zxyh.mapper.GcysSqdMapper;
+import com.hdsx.zxyh.service.GcysSqdService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+
+@Service
+public class GcysSqdServiceImpl implements GcysSqdService {
+
+    @Resource
+    private GcysSqdMapper gcysSqdMapper;
+
+    @Override
+    public List<Gcyssqd> getGcyssqdList(Map<String, String> param, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<Gcyssqd> list = gcysSqdMapper.getGcyssqdList(param);
+        return list;
+    }
+
+    @Override
+    public Gcyssqd getGcyssqdInfoBySqbh(Map<String, String> param) {
+        return gcysSqdMapper.getGcyssqdInfoBySqbh(param);
+    }
+
+    @Override
+    public int addGcyssqdForGcys(Gcyssqd gcyssqd) {
+        return gcysSqdMapper.addGcyssqdForGcys(gcyssqd);
+    }
+
+    @Override
+    public int editGcyssqdForGcys(Gcyssqd gcyssqd) {
+        return gcysSqdMapper.editGcyssqdForGcys(gcyssqd);
+    }
+
+    @Override
+    public int delGcyssqdForGcys(List<String> l) {
+        return gcysSqdMapper.delGcyssqdForGcys(l);
+    }
+
+
+}
